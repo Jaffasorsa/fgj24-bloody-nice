726d2a4f8054e0ded97e34e5cd72fe2dffe5aa67
Paul Standing <paultobiasstanding@gmail.com>
2024-01-28 00:18:51 +0200

    Work on the shop logic mostly
    - Purchasing takes money from you
    - Purchasing now checks if you have enough money to buy the thing
    - The upgrade system, though maybe not to be used is hopefully in place. You can only upgrade to the max level
    - Did a mass tidy up, renaming to try and be more clear, keeping methods/properties etc. protected unless need to be public.

--- Assets/_scripts/Shop.cs
+++ Assets/_scripts/Shop.cs
@@ -8,9 +8,12 @@ using UnityEngine.UI;
 public class Shop : MonoBehaviour
 {
 	[SerializeField]
-	protected List<ScriptableUpgradeInventoryItem> allUpgradeInventoryItems;
+	protected List<ScriptableUpgradeInventoryItem> upgradeInventoryInEditor = new();
 
-	public static Dictionary<string, UpgradeInventoryItem> OwnedUpgradeInventoryItems;
+	protected Dictionary<string, ScriptableUpgradeInventoryItem> upgradeInventory = new();
+
+	public static Dictionary<string, UpgradeInventoryItem> OwnedUpgrades = new();
+	public static float Money = 20000f;
 
 	[SerializeField]
 	protected GameObject buttonPrefab;
@@ -19,7 +22,12 @@ public class Shop : MonoBehaviour
 	protected Transform canvasRoot;
 
 	[SerializeField]
-	protected List<Button> visibleButtons = new List<Button>();
+	protected Dictionary<string, Button> visibleButtons = new();
+
+	public void Awake()
+	{
+		ConvertUpgradeInventoryItemsFromListToDictionary();
+	}
 
 	public void OnEnable()
 	{
@@ -33,17 +41,12 @@ public class Shop : MonoBehaviour
 
 	protected void OpenTheShop()
 	{
-		Debug.Log("Shop opens");
-
 		LoadData();
 		SetButtons();
-
 	}
 
 	protected void CloseTheShop()
 	{
-		Debug.Log("Shop closes");
-
 		SaveData();
 		DestroyButtons();
 	}
@@ -51,11 +54,11 @@ public class Shop : MonoBehaviour
 	/// <summary>
 	/// Sets the buttons on the screen
 	/// </summary>
-	public void SetButtons()
+	protected void SetButtons()
 	{
 		float currentScreenYPosition = Screen.height;
 
-		foreach (ScriptableUpgradeInventoryItem item in allUpgradeInventoryItems)
+		foreach (ScriptableUpgradeInventoryItem item in upgradeInventory.Values)
 		{
 			// Instantiation
 			GameObject buttonGameObject = Instantiate(buttonPrefab, canvasRoot);
@@ -65,10 +68,10 @@ public class Shop : MonoBehaviour
 			Button button = buttonGameObject.GetComponentInChildren<Button>();
 
 			// Add the button into the visible list
-			visibleButtons.Add(button);
+			visibleButtons[item.Name] = button;
 
 			// Name the button
-			textMeshProUGUI.text = item.Name;
+			textMeshProUGUI.text = $"{item.Name}\n{item.Price} ";
 
 			// Set up the click event
 			button.onClick.AddListener(delegate { PurchaseItem(item); });
@@ -78,11 +81,29 @@ public class Shop : MonoBehaviour
 			buttonGameObject.transform.position = new Vector3(rectTransform.sizeDelta.x * 0.5f, currentScreenYPosition, 0);
 			currentScreenYPosition -= rectTransform.sizeDelta.y * 0.5f;
 		}
+
+		CheckAndDisableButtonsIfNeeded();
 	}
 
-	public void DestroyButtons()
+	/// <summary>
+	/// Checks all the buttons to see if they need disabling
+	/// </summary>
+	protected void CheckAndDisableButtonsIfNeeded()
 	{
-		foreach (var button in visibleButtons)
+		foreach (var upgradeInventoryItem in upgradeInventory.Values)
+		{
+			// Disable if already purchased or not enough cash
+			if (Money < upgradeInventoryItem.Price || (OwnedUpgrades.ContainsKey(upgradeInventoryItem.Name) && OwnedUpgrades[upgradeInventoryItem.name]?.UpgradeLevel == upgradeInventoryItem.maxUpgradeLevel))
+				visibleButtons[upgradeInventoryItem.name].interactable = false;
+		}
+	}
+
+	/// <summary>
+	/// Destroy all the buttons and event subscriptions
+	/// </summary>
+	protected void DestroyButtons()
+	{
+		foreach (Button button in visibleButtons.Values)
 		{
 			button.onClick.RemoveAllListeners();
 			Destroy(button.gameObject);
@@ -95,23 +116,24 @@ public class Shop : MonoBehaviour
 	/// Purchase upgrade
 	/// </summary>
 	/// <param name="upgradeInventoryItem"></param>
-	public void PurchaseItem(ScriptableUpgradeInventoryItem upgradeInventoryItem)
+	protected void PurchaseItem(ScriptableUpgradeInventoryItem upgradeInventoryItem)
 	{
-		OwnedUpgradeInventoryItems[upgradeInventoryItem.name] = new UpgradeInventoryItem(upgradeInventoryItem.name, 0);
-	}
+		if (Money < upgradeInventoryItem.Price) return;
 
-	/// <summary>
-	/// TODO: Remove me. I am just for development purposes
-	/// </summary>
-	public void ShowPurchases()
-	{
-		string purchases = "";
-		foreach ((string s, UpgradeInventoryItem u) in OwnedUpgradeInventoryItems)
+		Money -= upgradeInventoryItem.Price;
+
+		// If the item is owned already, iterate the upgrade level
+		if (OwnedUpgrades.ContainsKey(upgradeInventoryItem.Name))
 		{
-			purchases += u.Name + ",";
+			OwnedUpgrades[upgradeInventoryItem.name].UpgradeLevel++;
+		}
+		// Otherwise just add to dictionary with level 1
+		else
+		{
+			OwnedUpgrades[upgradeInventoryItem.name] = new UpgradeInventoryItem(upgradeInventoryItem.name, 1);
 		}
 
-		Debug.Log(purchases);
+		CheckAndDisableButtonsIfNeeded();
 	}
 
 	/// <summary>
@@ -119,8 +141,9 @@ public class Shop : MonoBehaviour
 	/// </summary>
 	protected void SaveData()
 	{
-		string serializedListOfUpgrades = JsonConvert.SerializeObject(OwnedUpgradeInventoryItems);
+		string serializedListOfUpgrades = JsonConvert.SerializeObject(OwnedUpgrades);
 		PlayerPrefs.SetString("upgrades", serializedListOfUpgrades);
+		PlayerPrefs.SetString("money", Money.ToString());
 	}
 
 	/// <summary>
@@ -132,18 +155,43 @@ public class Shop : MonoBehaviour
 
 		if (upgradesFromPlayerPrefs == "")
 		{
-			OwnedUpgradeInventoryItems = new();
 			return;
 		}
 
 		try
 		{
-			OwnedUpgradeInventoryItems = JsonConvert.DeserializeObject<Dictionary<string, UpgradeInventoryItem>>(upgradesFromPlayerPrefs);
+			OwnedUpgrades = JsonConvert.DeserializeObject<Dictionary<string, UpgradeInventoryItem>>(upgradesFromPlayerPrefs);
 		}
 		catch (Exception e)
 		{
 			Debug.Log($"Some issue with getting the data, creating new dictionary for owned items: {e}");
-			OwnedUpgradeInventoryItems = new();
+			OwnedUpgrades = new();
 		}
+
+		float.TryParse(PlayerPrefs.GetString("money", ""), out Money);
+	}
+
+	protected void ConvertUpgradeInventoryItemsFromListToDictionary()
+	{
+		foreach (var item in upgradeInventoryInEditor)
+		{
+			upgradeInventory[item.Name] = item;
+		}
+	}
+
+	// DEVELOPMENT
+
+	/// <summary>
+	/// TODO: Remove me. I am just for development purposes
+	/// </summary>
+	protected void ShowPurchases()
+	{
+		string purchases = "";
+		foreach (string key in OwnedUpgrades.Keys)
+		{
+			purchases += key + ",";
+		}
+
+		Debug.Log(purchases);
 	}
 }
